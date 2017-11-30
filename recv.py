f2ce95b6695b370ca18f7016043dc7fc59093c0a
Nar Kumar Chhantyal <chtn@viessmann.com>
2017-11-30 15:05:30 +0100

    add app file

---
+++ recv.py
@@ -0,0 +1,13 @@
+from flask import Flask, request
+import json
+
+app = Flask(__name__)
+
+
+@app.route('/log', methods=["GET", 'POST'])
+def index():
+    print(json.dumps(request.form))
+    return "App is running."
+
+if __name__ == '__main__':
+    app.run(debug=True)
